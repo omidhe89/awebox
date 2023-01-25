a8697244cfdd509745ba8a6cab4a97afe13a1c17
Thomas Haas <thomash@cfdclu15.ugent.be>
2023-01-25 13:50:45 +0100

    modified mpc example for 6dof

--- examples/mpc_closed_loop.py
+++ examples/mpc_closed_loop.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-MPC-based closed loop simulation example for a single 3DOF kite system.
+MPC-based closed loop simulation example for a single 3DOF/6DOF kite system.
 """
 
 # imports
@@ -10,18 +10,24 @@ import copy
 import matplotlib.pyplot as plt
 #from awebox.logger.logger import Logger as awelogger
 #awelogger.logger.setLevel('DEBUG')
+MPC_6DOF_FLAG = True
 
 # single kite with point-mass model
 options = {}
 options['user_options.system_model.architecture'] = {1:0}
 options['user_options.kite_standard'] = awe.ampyx_data.data_dict()
 options['user_options.system_model.kite_dof'] = 3
+if MPC_6DOF_FLAG:
+    options['user_options.system_model.kite_dof'] = 6
 
 # trajectory should be a single pumping cycle
 options['user_options.trajectory.type'] = 'power_cycle'
 options['user_options.trajectory.system_type'] = 'lift_mode'
 options['user_options.trajectory.lift_mode.windings'] = 1
 options['model.system_bounds.theta.t_f'] =  [5.0, 15.0]
+if MPC_6DOF_FLAG:
+    options['model.system_bounds.theta.t_f'] =  [10.0, 30.0]
+    
 # wind model
 options['params.wind.z_ref'] = 10.0
 options['user_options.wind.model'] = 'log_wind'
@@ -38,7 +44,7 @@ options['solver.mu_hippo'] = 1e-2
 trial = awe.Trial(options, 'single_kite_lift_mode')
 trial.build()
 trial.optimize()
-trial.plot(['isometric'])
+#trial.plot(['isometric'])
 
 # set-up closed-loop simulation
 N_mpc = 10 # MPC horizon
@@ -71,5 +77,20 @@ options['sim.sys_params']['wind']['u_ref'] = 1.0*options['sim.sys_params']['wind
 # make simulator
 closed_loop_sim = awe.sim.Simulation(trial, 'closed_loop', ts, options)
 closed_loop_sim.run(N_sim)
-closed_loop_sim.plot(['isometric','states'])
+closed_loop_sim.plot(['states'])
+closed_loop_sim.plot(['isometric'])
+
+# adapt isometric plot
+fig = plt.gcf()
+fig.set_size_inches(10, 8)
+ax = fig.get_axes()[0]
+l = ax.get_lines()
+if (len(l) % 2 == 0):
+    n = int(len(l)/2)
+else:
+    n = int(len(l)/2) + 1
+l[0].set_color('r')
+l[n].set_color('b')
+ax.get_legend().remove()
+ax.legend([l[n], l[0]], ['ref', 'mpc'], fontsize=14)
 plt.show()
